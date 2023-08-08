e6588fee3335ed221787c1f9015c4e9e83f46e71
spiganvlad <spiganvlad@gmail.com>
2023-08-08 15:35:27 +0300

    Implement header component logic

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterLink } from "@angular/router";
+import { ConverterType } from "../../model/converter-type.enum";
+
 
 @Component({
     selector: "app-layout-header",
     templateUrl: "./header.component.html",
     styleUrls: ["./header.component.css"],
+    imports: [FormsModule, RouterLink],
     standalone: true
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+    public fileId: string | undefined;
+    public ConverterType = ConverterType;
+}
