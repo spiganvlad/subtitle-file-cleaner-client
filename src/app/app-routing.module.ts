0d9fdb9a83e9f92e2a28e51ea11f0289aa40a517
spiganvlad <spiganvlad@gmail.com>
2024-03-13 18:18:19 +0200

    Implement page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: "", loadComponent: () => import("./features/home/home.component").then((m => m.HomeComponent)) },
-  { path: "cleaner", loadComponent: () => import("./features/cleaner/cleaner.component").then((m => m.CleanerComponent)) },
-  { path: "converted", loadComponent: () => import("./features/converted-file/converted-file.component").then(m => m.ConvertedFileComponent) }
+  { path: "", loadComponent: () => import("./features/home/home.component").then(m => m.HomeComponent) },
+  { path: "cleaner", loadComponent: () => import("./features/cleaner/cleaner.component").then(m => m.CleanerComponent) },
+  { path: "converted", loadComponent: () => import("./features/converted-file/converted-file.component").then(m => m.ConvertedFileComponent) },
+  { path: "**", pathMatch: "full", loadComponent: () => import("./features/page-not-found/page-not-found.component").then(m => m.PageNotFoundComponene) }
 ];
 
 @NgModule({
